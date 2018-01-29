96c346bc1ae2453460245e14abd4c631edb42dc6
Dae young Ko <logoskdy@naver.com>
2018-01-29 10:15:08 +0900

    Initial Commit

---
+++ Calendar/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Calendar
+//
+//  Created by 고대영 on 2018. 1. 29..
+//  Copyright © 2018년 고대영. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
